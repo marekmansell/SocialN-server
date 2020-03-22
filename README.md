3fedda6f076e0780229c4ee9a4cff0ad34f7c4d6
Marek Mansell <marek.mansell@gmail.com>
2020-03-22 22:28:11 +0100

    connected to db

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 
+Before run:
+
+```
+export MTAA_DB_ADDR="localhost"
+export MTAA_DB_PORT="5432"
+export MTAA_DB_USER="postgres"
+export MTAA_DB_PASS="password"
+export MTAA_DB_DB="postgres"
+```
+
+```
+export FLASK_APP=main.py
+export FLASK_ENV=development
+```
+
+```
+flask run
+```
--- app/views.py
+++ app/views.py
@@ -1,10 +1,35 @@
 from app import app
 from flask import render_template
+import psycopg2
 import os
+
+conn = psycopg2.connect(
+		database=os.environ['MTAA_DB_DB'],
+		user=os.environ['MTAA_DB_USER'],
+		password=os.environ['MTAA_DB_PASS'],
+		host=os.environ['MTAA_DB_ADDR'],
+		port=os.environ['MTAA_DB_PORT']
+	)
 
 @app.route('/')
 def home():
-   return "{}, {}, {}".format(os.environ['MTAA_DB_ADDR'], os.environ['MTAA_DB_PORT'], os.environ['MTAA_DB_DB'])
+	cur = conn.cursor()
+
+	cur.execute("SELECT id, name, address, salary  from COMPANY")
+	rows = cur.fetchall()
+
+
+	html = [" ".join([str(x).strip() for x in row]) for row in rows]
+
+	return "<br>".join(html)
+
+@app.route('/add/')
+def add():
+	cur = conn.cursor()
+	cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+	      VALUES (9, 'Allen', 25, 'Texas', 15000.00 )")
+
+	conn.commit()
 
 @app.route('/template')
 def template():
