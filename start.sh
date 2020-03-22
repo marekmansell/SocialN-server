40e368ec26db8f2112f944d5caf5874a6eed12fe
Marek Mansell <marek.mansell@gmail.com>
2020-03-22 21:09:01 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@ app="mtaa.docker"
 docker build -t ${app} .
 docker run -d -p 5001:80 \
   --name=${app} \
+  --env MTAA_DB_ADDR=${MTAA_DB_ADDR}
+  --env MTAA_DB_PORT=${MTAA_DB_PORT}
+  --env MTAA_DB_USER=${MTAA_DB_USER}
+  --env MTAA_DB_PASS=${MTAA_DB_PASS}
+  --env MTAA_DB_DB=${MTAA_DB_DB}
   ${app}
