c609f71424d318742ae12fdbaed33a2017e580e2
Marek Mansell <marek.mansell@gmail.com>
2020-03-22 21:13:00 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import render_template
+import os
 
 @app.route('/')
 def home():
-   return "<b>Success :-)</b>"
+   return "{}, {}, {}".format(os.environ['MTAA_DB_ADDR'], os.environ['MTAA_DB_PORT'], os.environ['MTAA_DB_DB'])
 
 @app.route('/template')
 def template():
